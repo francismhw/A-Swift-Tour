90dabd4e66645f118531ab845d1a1374e29a6f36
Francis White <francismhw@gmail.com>
2014-06-02 19:05:02 -0500

    working through the swift tour

---
+++ GuidedTour.playground/section-11.swift
@@ -0,0 +1,8 @@
+let apples = 3
+let oranges = 5
+let appleSummary = "I have \(apples) apples."
+let fruitSummary = "I have \(apples + oranges) pieces of fruit."
+let name = "Frank"
+let num0 = 3.589
+let num1 = 4.999
+let nameSummary = "Hello, my name is \(name). The sliced cajun ham you ordered weighs \(num0 + num1) lbs."
---
+++ GuidedTour.playground/section-9.swift
@@ -0,0 +1,4 @@
+let label = "The width is "
+let width = 94
+let widthLabel = label + String(width)
+//Could not find and overload for '+' that accepts the supplied arguments
